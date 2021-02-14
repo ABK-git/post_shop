9adc2f110b94b8f0fd9f8c011d2d195cf46d45b3
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-02-14 20:32:54 +0900

    logout機能を実装

--- apollo/actions/index.js
+++ apollo/actions/index.js
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useLazyQuery } from "@apollo/react-hooks";
-import { SIGN_IN, SIGN_UP, GET_USER } from "../queries";
+import { SIGN_IN, SIGN_UP, GET_USER, SIGN_OUT } from "../queries";
 
 //User認証
 export const userSignUp = () => useMutation(SIGN_UP);
@@ -14,3 +14,4 @@ export const userSignIn = () =>
   });
 export const getLazyAuthUser = () => useLazyQuery(GET_USER);
 export const getAuthUser = () => useQuery(GET_USER);
+export const userSignOut = () => useMutation(SIGN_OUT);
