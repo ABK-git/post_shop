6227b4c6256ba0f681145d49e80c01ad17dc3b20
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-02-20 10:03:57 +0900

    表示するpreview画像をボタンで変更可能に

--- components/display-product-images/product-images.styles.jsx
+++ components/display-product-images/product-images.styles.jsx
@@ -1,15 +1,37 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "tailwind.macro";
-import { ChevronLeftCircle, ChevronRightCircle } from "@styled-icons/boxicons-regular";
+import {
+  ChevronLeftCircle,
+  ChevronRightCircle,
+} from "@styled-icons/boxicons-regular";
+
+const setVisible = ({ getVisibility }) => {
+  if (getVisibility) {
+    return invisible;
+  } else {
+    return visible;
+  }
+};
+
+const visible = css`
+  ${tw`visible`}
+`;
+const invisible = css`
+  ${tw`invisible`}
+`;
 
 export const ProductImagesContainer = styled.div`
   ${tw`flex h-60`}
 `;
 
 export const ChevronRightButton = styled(ChevronRightCircle)`
-  ${tw` w-10 `}
+  ${tw` w-10 text-blue-600 hover:text-red-600`}
+
+  ${setVisible}
 `;
 
 export const ChevronLeftButton = styled(ChevronLeftCircle)`
-  ${tw` w-10`}
+  ${tw` w-10 text-blue-600 hover:text-red-600`};
+
+  ${setVisible}
 `;
