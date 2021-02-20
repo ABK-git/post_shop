8806cd01a3f8a5fb48af088ca0afd4c7bf7c8323
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-02-20 11:32:25 +0900

    preview画像のhoverに画像の削除ボタン表示を設定

--- components/display-product-images/product-images.component.jsx
+++ components/display-product-images/product-images.component.jsx
@@ -4,8 +4,10 @@ import {
   ChevronLeftButton,
   ChevronRightButton,
   ImageIndex,
+  ResizeImagesContainer,
+  RemoveImageButton,
+  ResizeImagesAndRemoveButton
 } from "./product-images.styles";
-import Image from "react-image-resizer";
 import MyContext from "../../context";
 
 const DisplayProductImages = ({ images }) => {
@@ -28,7 +30,7 @@ const DisplayProductImages = ({ images }) => {
     }
   };
   //画像の番号
-  const getImageIndex = (index+1) + "/"+ (images.length);
+  const getImageIndex = index + 1 + "/" + images.length;
 
   return (
     <div>
@@ -37,11 +39,15 @@ const DisplayProductImages = ({ images }) => {
           onClick={handleClickLeftButton}
           getVisibility={index === 0}
         />
-        <Image
-          src={URL.createObjectURL(images[index])}
-          width={smBreakPoint ? 250 : 160}
-          height={smBreakPoint ? 200 : 130}
-        />
+        <ResizeImagesAndRemoveButton>
+          <ResizeImagesContainer
+            src={URL.createObjectURL(images[index])}
+            width={smBreakPoint ? 250 : 160}
+            height={smBreakPoint ? 200 : 130}
+          />
+          <RemoveImageButton>Remove</RemoveImageButton>
+        </ResizeImagesAndRemoveButton>
+
         <ChevronRightButton
           onClick={handleClickRightButton}
           getVisibility={index === images.length - 1}
--- components/display-product-images/product-images.styles.jsx
+++ components/display-product-images/product-images.styles.jsx
@@ -4,6 +4,7 @@ import {
   ChevronLeftCircle,
   ChevronRightCircle,
 } from "@styled-icons/boxicons-regular";
+import Image from "react-image-resizer";
 
 const setVisible = ({ getVisibility }) => {
   if (getVisibility) {
@@ -12,7 +13,6 @@ const setVisible = ({ getVisibility }) => {
     return visible;
   }
 };
-
 const visible = css`
   ${tw`visible`}
 `;
@@ -20,8 +20,29 @@ const invisible = css`
   ${tw`invisible`}
 `;
 
+export const ResizeImagesAndRemoveButton = styled.div`
+  ${tw`flex h-60 items-center justify-center relative`}
+
+  &:hover {
+    button {
+      display: flex;
+    }
+  }
+`;
+
 export const ProductImagesContainer = styled.div`
   ${tw`flex h-60`}
+`;
+
+export const RemoveImageButton = styled.button`
+  ${tw`m-auto hidden flex-col justify-center items-center absolute
+  font-bold text-xl sm:font-black sm:text-4xl bg-red-500 rounded-lg
+  hover:bg-black hover:text-red-500
+  `}
+`;
+
+export const ResizeImagesContainer = styled(Image)`
+  ${tw`flex`}
 `;
 
 export const ChevronRightButton = styled(ChevronRightCircle)`
