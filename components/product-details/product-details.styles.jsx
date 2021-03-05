f8cf253d26e7492735fb25b960163ab3376d2d93
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-05 18:48:23 +0900

    ProductDetailsのProduct画像のレスポンシブを修正

--- components/product-details/product-details.styles.jsx
+++ components/product-details/product-details.styles.jsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 
 export const ProductDetailsContainer = styled.div`
-  ${tw`h-screen w-full text-center pb-5 overflow-auto bg-gray-500`}
+  ${tw`h-screen w-full py-6 flex flex-col items-center bg-gray-500 overflow-auto`}
 `;
 
 export const DetailsMain = styled.div`
-  ${tw`py-10 m-0 m-auto inline-block`}
+  ${tw`pt-10`}
 `;
