1f8da8e60b0b112f36e7bacb349b6c006ba7278f
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-08 12:12:39 +0900

    画面遷移しても絞り込み条件をリセットしないようにした。

--- context/reducer.js
+++ context/reducer.js
@@ -1,4 +1,5 @@
 import ContextTypes from "./types";
+import { changeFilterFromEvent, setFilterFromQuery } from "./utils";
 
 const Reducer = (state, action) => {
   switch (action.type) {
@@ -6,6 +7,16 @@ const Reducer = (state, action) => {
       return {
         ...state,
         displayMenu: !state.displayMenu,
+      };
+    case ContextTypes.CHANGE_FILTER:
+      return {
+        ...state,
+        filterState: changeFilterFromEvent(state.filterState, action.payload),
+      };
+    case ContextTypes.SET_FILTER_FROM_QUERY:
+      return {
+        ...state,
+        filterState: setFilterFromQuery(state.filterState, action.payload),
       };
     default:
       return state;
