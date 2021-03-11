9c1e5f68bd30b29aba75c927684512bc6e0e5ab4
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-11 20:06:44 +0900

    QuestionDetailsページに質問内容と返信入力フォームを設定

--- components/question-details/question-details.styles.jsx
+++ components/question-details/question-details.styles.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import tw from "tailwind.macro";
+import TextareaInput from "../textarea-input/textarea-input.component";
 
 export const QuestionDetailsContainer = styled.div`
   ${tw`w-full pt-5`}
@@ -7,4 +8,28 @@ export const QuestionDetailsContainer = styled.div`
 
 export const MainContent = styled.div`
   ${tw`w-full pt-5 text-center`}
+`;
+
+export const QuestionBody = styled.div`
+  ${tw`w-4/5 mx-auto mt-3 pb-3 border-2 border-gray-400`}
+`;
+
+export const QuestionTitle = styled.p`
+  ${tw`font-bold text-3xl`}
+`;
+
+export const QuestionUser = styled.p`
+  ${tw`flex justify-start text-gray-600`}
+`;
+
+export const FlexDiv = styled.div`
+  ${tw`flex mb-3`}
+`;
+
+export const ReplyContainer = styled.form`
+  ${tw`w-4/5 mx-auto`};
+`;
+
+export const ReplyTextareaInput = styled(TextareaInput)`
+  ${tw`w-full border-2 border-gray-400 mt-3`}
 `;
