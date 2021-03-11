8882676e7a444b26a60d230467b6bfcbfdecab4d
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-11 20:06:44 +0900

    QuestionDetailsページに質問内容と返信入力フォームを設定

--- components/question-details/question-details.page.jsx
+++ components/question-details/question-details.page.jsx
@@ -1,9 +1,15 @@
 import { useRouter } from "next/dist/client/router";
-import React from "react";
+import React, { useState } from "react";
 import CustomButton from "../custom-button/custom-button.component";
+import moment from "moment";
 import {
-  MainContent,
+  QuestionBody,
   QuestionDetailsContainer,
+  QuestionTitle,
+  QuestionUser,
+  FlexDiv,
+  ReplyContainer,
+  ReplyTextareaInput,
 } from "./question-details.styles";
 
 const QuestionDetails = ({ question }) => {
@@ -15,18 +21,54 @@ const QuestionDetails = ({ question }) => {
       query: { openQuestions: true },
     });
   };
+
+  let new_content = [];
+  //question.contentを改行で分割
+  if (question.content.includes("\n")) {
+    new_content = question.content.split("\n");
+  } else {
+    new_content.push(question.content);
+  }
+  const [reply, setReply] = useState("");
+  const handleChange = (event) => {
+    setReply(event.target.value);
+  };
+  const handleSubmit = () => {
+    console.log("handleSubmit");
+    console.log(reply);
+  };
+
   return (
     <QuestionDetailsContainer>
-      <CustomButton onClick={clickToProductDetails} design="to_list_and_margin-left">
+      <CustomButton
+        onClick={clickToProductDetails}
+        design="to_list_and_margin-left">
         質問一覧に戻る
       </CustomButton>
-      <MainContent>
-        <p>{question.title}</p>
-        <p>{question.content}</p>
-        <p>{question.createdAt}</p>
-        <p>{question.user.username}</p>
-        <p>{question.product._id}</p>
-      </MainContent>
+      <QuestionBody>
+        <QuestionTitle>{question.title}</QuestionTitle>
+        <FlexDiv>
+          <QuestionUser>質問者:{question.user.username}</QuestionUser>・
+          <p>{moment(parseInt(question.createdAt)).fromNow()}</p>
+        </FlexDiv>
+        {new_content && new_content.map((content) => <p>{content}</p>)}
+      </QuestionBody>
+
+      <ReplyContainer onSubmit={handleSubmit}>
+        <ReplyTextareaInput
+          name="reply"
+          placeholder="返信を追加"
+          maxLength="1000"
+          rows="3"
+          value={reply}
+          handleChange={handleChange}
+        />
+        {reply && (
+          <CustomButton design="reply_to_question" type="submit">
+            公開
+          </CustomButton>
+        )}
+      </ReplyContainer>
     </QuestionDetailsContainer>
   );
 };
