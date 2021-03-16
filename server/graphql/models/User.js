730bd9ad8e386772bc065ea18863b6195223f505
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-16 17:14:03 +0900

    User情報のUpdate(サーバーサイド)

--- server/graphql/models/User.js
+++ server/graphql/models/User.js
@@ -19,6 +19,38 @@ class User {
       return new Error("User登録に失敗しました");
     }
   }
+  async updateUser(updateData, ctx) {
+    const { username, email, password, password_confirm, avatar } = updateData;
+    const fs = require("fs");
+    if (password !== password_confirm) {
+      throw new Error("Passwordを確認してください");
+    }
+    //ログインユーザーの取得
+    let oldAvatar = "";
+    const id = ctx.getUser();
+    const user = await this.Model.findById(id);
+    if(!user){
+      throw new Error("ログインしてください");
+    }
+    //古いavatarを取得
+    oldAvatar = user.avatar;
+    //編集
+    user.username = username;
+    user.email = email;
+    user.password = password;
+    user.avatar = avatar;
+    //update
+    await user.save();
+    //古いUser画像を削除
+    if (oldAvatar && avatar) {
+      fs.unlink(`./public${oldAvatar}`, (err) => {
+        if(err){
+          console.log("ファイル削除失敗")
+        }
+      });
+    }
+    return user;
+  }
   async signIn(signInData, ctx) {
     try {
       return await ctx.authenticate(signInData); //ここ
