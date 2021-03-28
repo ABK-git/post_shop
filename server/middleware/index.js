a0f8de51fb0b48d8c3a1d1ecdd5eaa9ff5f615bb
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-28 15:11:18 +0900

    Deployment to Heroku

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,4 +1,4 @@
-const config = require("../config/dev");
+const config = require("../config");
 const session = require("express-session");
 const passport = require("passport");
 
@@ -13,8 +13,18 @@ exports.init = (server, db) => {
     saveUninitialized: false,
     store: db.initSessionStore(),
   };
+  //本番環境の場合
+  if (process.env.NODE_ENV === "production") {
+    server.set("trust proxy",1);
+    sess.cookie.secure = true;
+    sess.cookie.httpOnly = true;
+    sess.cookie.sameSite = true;
+    sess.cookie.domain = process.env.DOMAIN;
+  }
 
   server.use(session(sess));
   server.use(passport.initialize());
   server.use(passport.session());
 };
+
+// .post-shop1.herokuapp.com/
