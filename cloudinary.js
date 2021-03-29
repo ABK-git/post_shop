683cce8fc0e4adad5968c5a73f7ec3a7653bc6d0
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-03-29 20:21:57 +0900

    cloudinary本番環境調整

---
+++ cloudinary.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CLOUDINARY_UPLOAD_IMAGE_URL:
+    "https://api.cloudinary.com/v1_1/nojob/image/upload",
+  CLOUDINARY_UPLOAD_PRESET: "fsbv8i7r",
+};
--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
+const cloudinary = require("./cloudinary");
 
+require("dotenv").config();
 //localか本番環境か否か(localでtrue)
 const dev = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com","localhost"],
+    domains: ["res.cloudinary.com", "localhost"],
   },
   env: {
     BASE_URL: dev
       ? "http://localhost:3000/graphql"
       : "https://post-shop1.herokuapp.com/graphql",
+    UPLOAD_IMAGE_URL: dev
+      ? cloudinary.CLOUDINARY_UPLOAD_IMAGE_URL
+      : process.env.CLOUDINARY_UPLOAD_IMAGE_URL,
+    UPLOAD_PRESET: dev
+      ? cloudinary.CLOUDINARY_UPLOAD_PRESET
+      : process.env.CLOUDINARY_UPLOAD_PRESET,
   },
 };
 
